f3bdd4f9c388b545e1b4e023d7769587e95082f3
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-05-27 16:47:57 +0900

    [Add] 空ページアクセス時のリダイレクトルートを設定

--- vuetify-project/src/router/index.js
+++ vuetify-project/src/router/index.js
@@ -10,7 +10,14 @@ import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  extendRoutes: setupLayouts,
+  extendRoutes: routes => [
+    {
+      path: '/:paths(.*)*',
+      name: 'nothing',
+      redirect: '/home'
+    },
+    ...setupLayouts(routes),
+  ],
 })
 
 export default router
