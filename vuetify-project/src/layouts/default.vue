4cd57a1a2e4ed6a11c8bab47077d578d20160caa
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2024-05-28 12:52:39 +0900

    [SAVE] #1

--- vuetify-project/src/layouts/default.vue
+++ vuetify-project/src/layouts/default.vue
@@ -1,5 +1,25 @@
 <template>
   <v-app>
+    <h2 class="text-h2">Reading Recorder</h2>
+
+    <nav>
+      <ul class="d-flex">
+        <li>
+          <router-link to="/home">
+            Home
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/search">
+            Search
+          </router-link>
+        </li>
+        <li>
+          Support
+        </li>
+      </ul>
+    </nav>
+
     <v-main>
       <router-view />
     </v-main>
